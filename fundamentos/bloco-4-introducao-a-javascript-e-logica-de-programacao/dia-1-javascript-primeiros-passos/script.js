cebe47b85c69b69ab4a1f40bb17e366de28de813
Thaysa <thay.fernnanda@gmail.com>
2022-02-09 18:45:35 -0300

    adiciona retorna maior de dois numeros

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,12 +1,21 @@
-//1 
+
 
 const a = 5;
 const b = 10;
 
-console.log('Soma: ' + (a+b));
-console.log('Subtração: ' + (a-b));
-console.log('Multiplicação: ' + (a*b));
-console.log('Divisão: ' + (a/b));
-console.log('Módulo: ' + (a%b));
+//1 
 
+// console.log('Soma: ' + (a+b));
+// console.log('Subtração: ' + (a-b));
+// console.log('Multiplicação: ' + (a*b));
+// console.log('Divisão: ' + (a/b));
+// console.log('Módulo: ' + (a%b));
 
+//2
+
+if (a > b){
+    console.log(a + ' é maior que '  + b);
+}
+else{
+    console.log(b + ' é maior que ' + a);
+}

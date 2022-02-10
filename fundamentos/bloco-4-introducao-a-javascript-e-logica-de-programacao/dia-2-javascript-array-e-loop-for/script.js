431170d00674f4a56e63f42550e131ca07f37f2a
Thaysa <thay.fernnanda@gmail.com>
2022-02-10 18:23:17 -0300

    adiciona soma

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,6 +1,12 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for(let index = 0; index < numbers.length; index +=1){
-    console.log(numbers[index]);
-}
+// for(let index = 0; index < numbers.length; index +=1){
+//     console.log(numbers[index]);
+// }
 
+let soma = 0;
+
+for(let index = 0; index <numbers.length; index +=1){
+    soma += numbers[index];
+}
+console.log(soma);

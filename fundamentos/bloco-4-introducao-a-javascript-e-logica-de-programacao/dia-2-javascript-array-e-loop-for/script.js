6128f3ca2d73892491bfcdfea112f72d1ca837f5
Thaysa <thay.fernnanda@gmail.com>
2022-02-10 18:34:32 -0300

    adiciona if

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,4 +1,4 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [5, 9, 3, 19, 5, 8, 50, 2, 35, 27];
 
 // for(let index = 0; index < numbers.length; index +=1){
 //     console.log(numbers[index]);
@@ -12,3 +12,10 @@ for(let index = 0; index <numbers.length; index +=1){
 
 let media = soma / numbers.length
 console.log(media);
+
+if (media > 20){
+    console.log('Valor maior que 20');
+}
+else{
+    console.log('Valor menor ou igual a 20');
+}
